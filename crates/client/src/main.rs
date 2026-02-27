7918b4608731e5a23f99fe30ef21ede67307fcf0
ntikhonov <ntikhonov@rubackup.ru>
2026-02-27 15:22:58 +0300

    Измен подход к логированию клиента

--- crates/client/src/main.rs
+++ crates/client/src/main.rs
@@ -12,6 +12,10 @@ use anyhow::{Result, bail};
 use clap::Parser;
 use std::path::PathBuf;
 
+use fern::Dispatch;
+use log::{Level, LevelFilter};
+use std::fs::OpenOptions;
+
 #[derive(Debug, Parser)]
 #[command(
     name = "quote_client",
@@ -56,9 +60,46 @@ fn read_tickers(file_name: &PathBuf) -> Result<HashSet<String>, ErrType> {
     Ok(tickers)
 }
 
-fn main() -> Result<()> {
-    env_logger::init();
+fn setup_logger(port: u16) -> Result<(), Box<dyn std::error::Error>> {
+    // Общий формат
+    let base_config = Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "[{}][{}][{}] {}",
+                chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
+                record.level(),
+                record.target(),
+                message
+            ))
+        })
+        .level(LevelFilter::Debug); // максимально возможный уровень
 
+    let file_log_name = format!("client_{}.log", port);
+
+    let file_logger = Dispatch::new()
+        .level(LevelFilter::Debug)
+        .chain(
+            OpenOptions::new()
+                .create(true)
+                .write(true)
+                .truncate(true) // очистим старые логи при старте
+                .open(file_log_name)?,
+        );
+
+    // В консоль попадает только Error все остальное в файл
+    let console_logger = Dispatch::new()
+        .filter(|metadata| metadata.level() == Level::Error)
+        .chain(std::io::stderr());
+
+    base_config
+        .chain(file_logger)
+        .chain(console_logger)
+        .apply()?;
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
     let cli = Cli::parse();
 
     let tickers = match read_tickers(&cli.tickers_file) {
@@ -120,6 +161,10 @@ fn main() -> Result<()> {
     let stoper = match common_lib::ctrlc::ctrlc_handler() {
         Ok(stoper) => stoper,
         Err(e) => bail!(e.to_string()),
+    };
+
+    let Ok(_) = setup_logger(cli.client_port) else {
+        bail!("Не удалось запустить логер")
     };
 
     let thread_handler;
